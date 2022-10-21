13fd49975ef515e32a11f8aca2e589d6b7be9ba1
Marcin Bigos <marcin.bigos99@gmail.com>
2022-10-21 22:46:49 +0200

    wrap instead grid

--- client/side-projects-it/src/Pages/Ideas/Ideas.tsx
+++ client/side-projects-it/src/Pages/Ideas/Ideas.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from '@chakra-ui/react'
+import { Wrap } from '@chakra-ui/react'
 import { FC } from 'react'
 import { Idea } from '../../Components/Idea/Idea'
 import { IdeasMock } from '../../Mocks/IdeasMock'
@@ -6,10 +6,10 @@ import { IdeasMock } from '../../Mocks/IdeasMock'
 export const Ideas: FC = () => {
   const mockData = IdeasMock
   return (
-    <SimpleGrid columns={5} spacing={10}>
+    <Wrap spacing={10}>
       {mockData.map((x) => (
         <Idea key={x.id} ideaDto={x} />
       ))}
-    </SimpleGrid>
+    </Wrap>
   )
 }
