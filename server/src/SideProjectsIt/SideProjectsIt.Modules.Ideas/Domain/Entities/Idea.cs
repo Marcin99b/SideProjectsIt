0149bc3e2790b072e0c8fc90cbfb4c8e18462bd4
Marcin Bigos <marcin.bigos99@gmail.com>
2022-10-28 20:49:42 +0200

    save

---
+++ server/src/SideProjectsIt/SideProjectsIt.Modules.Ideas/Domain/Entities/Idea.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideProjectsIt.Modules.Ideas.Domain.Entities
+{
+    public record Idea
+    {
+    }
+}
